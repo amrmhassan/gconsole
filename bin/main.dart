2429db94daa60e22ac1ada1f18864cd165fa680c
amrhassan <amrhassanpersonal@gmail.com>
2023-04-20 16:57:51 +0200

    dd

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,10 @@
-void main(List<String> arguments) {
-  print('this is the updated version');
+void main(List<String> arguments) async {
+  DateTime before = DateTime.now();
+  int sum = 0;
+  for (var i = 0; i < 1000000000; i++) {
+    sum += i;
+  }
+  print(sum);
+  DateTime after = DateTime.now();
+  print('took ${after.difference(before).inMilliseconds / 1000}ms');
 }
