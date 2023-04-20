a6843839b0cb89bea19ebbff5b015ff4d2f7119a
amrhassan <amrhassanpersonal@gmail.com>
2023-04-20 16:19:22 +0200

    d

--- bin/gconsole.dart
+++
@@ -1,5 +0,0 @@
-import 'package:gconsole/gconsole.dart' as gconsole;
-
-void main(List<String> arguments) {
-  print('Hello world: ${gconsole.calculate()}!');
-}
--- test/gconsole_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:gconsole/gconsole.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
